e6e6c2cbe796982b5c928284e0e126fea42572ac
Sanjeev Kumar <sanjeevcse2k23@gmail.com>
2024-01-15 12:27:02 +0530

    done

--- counter-button/src/App.js
+++ counter-button/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-import { FaPlusCircle } from "react-icons/fa";
-import { FaMinusCircle } from "react-icons/fa";
+
 import './App.css'
 
 const App = () => {
     const [count, setCount] = useState(0)
 
     const countminus = () => {
-      setCount (count-1)
+      if (count > 0) {
+        setCount(count - 1);
+      }
     }
     const countplush = () => {
       setCount (count+1)
@@ -15,11 +16,11 @@ const App = () => {
 
   return (
     <div className='App'>
+      <h1>{count}</h1>
+      <button  onClick={countminus}>Minus</button>
+      <button  onClick={countplush}>plush</button>
     
-      <FaMinusCircle onClick={countminus} />
-      <span>  {count} </span>
-      <FaPlusCircle onClick={countplush} />
-
+      
     </div>
 
 
